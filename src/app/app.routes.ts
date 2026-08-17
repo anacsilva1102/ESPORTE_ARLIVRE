b87296984dd6e714e48e08abb7f78eb893988da0
Ana Clara <ana.c.silva1102@aluno.senai.br>
2026-08-17 16:48:28 -0300

    projeto esporte ar livre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './component/home-component/home-component';
 import { AtletaComponent } from './component/atleta-component/atleta-component';
+import { AtletaListComponent } from './component/atleta-list-component/atleta-list-component';
 import { CorridaComponent } from './component/corrida-component/corrida-component';
 import { CorridasComponent } from './component/corridas-component/corridas-component';
 import { InscricoesComponent } from './component/inscricoes-component/inscricoes-component';
@@ -16,6 +17,11 @@ export const routes: Routes = [
     {
         path: 'cadastroatleta',
         component: AtletaComponent
+    },
+
+    {
+        path: 'listaatletas',
+        component: AtletaListComponent
     },
 
     {
